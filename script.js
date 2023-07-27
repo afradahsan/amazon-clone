26fd3aeb49ed336f587482cbaf5b7e51d5f3c6d9
Afrad Ahsan <afradahsan2002@gmail.com>
2023-07-27 12:58:56 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
 const products=[{
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  id: "s23-ultra-123456789",
+  image: "https://m.media-amazon.com/images/I/71Sa3dqTqzL._AC_UY327_FMwebp_QL65_.jpg",
+  name: 'SAMSUNG Galaxy S23 Ultra Cell Phone, Phantom Black',
   rating: {
     stars: 4.5,
     count: 87
   },
-  priceCents: 1090
+  priceCents: 99990
 }, {
-  image: "images/products/intermediate-composite-basketball.jpg",
-  name: 'Intermediate Size Basketball',
+  id: "jordan-black-white-123456789",
+  image: "https://m.media-amazon.com/images/I/51q8mx4il2L._AC_UL600_FMwebp_QL65_.jpg",
+  name: 'Jordan Mens Retro 6 - Hare Neutral Grey/Black-White',
   rating: {
-    stars: 4,
-    count: 127
+    stars: 5,
+    count: 417
   },
-  priceCents: 2095
+  priceCents: 24550
 }, {
-  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: "Adults Plain Cotton T-Shirt - 2 Pack",
+  id: "north-face-tee-white-123456789",
+  image: "https://m.media-amazon.com/images/I/61c64ekKcTL._AC_UL600_QL65_.jpg",
+  name: "THE NORTH FACE Men's Short Sleeve Box NSE Tee",
   rating: {
     stars: 4.5,
     count: 56
   },
-  priceCents: 799
-}];
+  priceCents: 2995
+},
+  {
+    id: "blender-smoothie-11pc-123456789",
+    image: "https://m.media-amazon.com/images/I/61qATnpHduL._AC_UY327_FMwebp_QL65_.jpg",
+    name: 'Magic Bullet Blender, Small, Silver, 11 Piece Set',
+    rating: {
+      stars: 4,
+      count: 124
+    },
+    priceCents: 3895
+  },
+  {
+    id: "go-pro-11-black-123456789",
+    image: "https://m.media-amazon.com/images/I/617sGSjmC1L._AC_SY450_.jpg",
+    name: 'GoPro HERO11 Black - Waterproof Action Camera with 5.3K60 Ultra HD Video',
+    rating: {
+      stars: 4,
+      count: 124
+    },
+    priceCents: 39700
+  },
+  {
+    id: "ikigai-prod-book-123456789",
+    image: "https://m.media-amazon.com/images/I/41mtUoTi8ZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    name: 'Ikigai: The Japanese Secret to a Long and Happy Life',
+    rating: {
+      stars: 4.5,
+      count: 5200
+    },
+    priceCents: 3895
+  },
+  {
+    id: "garnier-charcoal-scrub-123456789",
+    image: "https://m.media-amazon.com/images/I/41hryFSg58L._SX300_SY300_QL70_FMwebp_.jpg",
+    name: 'Garnier SkinActive Charcoal Blackhead Eliminating Scrub',
+    rating: {
+      stars: 4,
+      count: 124
+    },
+    priceCents: 759
+  }
+
+];
 
 let productsHTML = ``;
 
@@ -72,13 +117,49 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
   `
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let  matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem=item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      }
+      else{
+        cart.push({
+          productId: productId, 
+          quantity: 1
+        });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
+
+
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity
+ 
+  })
+  
+});
+
+const cart = [];
