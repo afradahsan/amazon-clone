1bb533b2c963a87d936cde4278c16f887fb5d4a1
Afrad Ahsan <afradahsan2002@gmail.com>
2023-07-27 13:54:13 +0530

    Index

--- script.js
+++ script.js
@@ -65,7 +65,76 @@ const products=[{
       count: 124
     },
     priceCents: 759
+  },
+{
+  id: "s23-ultra-123456789",
+  image: "https://m.media-amazon.com/images/I/71Sa3dqTqzL._AC_UY327_FMwebp_QL65_.jpg",
+  name: 'SAMSUNG Galaxy S23 Ultra Cell Phone, Phantom Black',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 99990
+}, {
+  id: "jordan-black-white-123456789",
+  image: "https://m.media-amazon.com/images/I/51q8mx4il2L._AC_UL600_FMwebp_QL65_.jpg",
+  name: 'Jordan Mens Retro 6 - Hare Neutral Grey/Black-White',
+  rating: {
+    stars: 5,
+    count: 417
+  },
+  priceCents: 24550
+}, {
+  id: "north-face-tee-white-123456789",
+  image: "https://m.media-amazon.com/images/I/61c64ekKcTL._AC_UL600_QL65_.jpg",
+  name: "THE NORTH FACE Men's Short Sleeve Box NSE Tee",
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 2995
+},
+  {
+    id: "blender-smoothie-11pc-123456789",
+    image: "https://m.media-amazon.com/images/I/61qATnpHduL._AC_UY327_FMwebp_QL65_.jpg",
+    name: 'Magic Bullet Blender, Small, Silver, 11 Piece Set',
+    rating: {
+      stars: 4,
+      count: 124
+    },
+    priceCents: 3895
+  },
+  {
+    id: "go-pro-11-black-123456789",
+    image: "https://m.media-amazon.com/images/I/617sGSjmC1L._AC_SY450_.jpg",
+    name: 'GoPro HERO11 Black - Waterproof Action Camera with 5.3K60 Ultra HD Video',
+    rating: {
+      stars: 4,
+      count: 124
+    },
+    priceCents: 39700
+  },
+  {
+    id: "ikigai-prod-book-123456789",
+    image: "https://m.media-amazon.com/images/I/41mtUoTi8ZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    name: 'Ikigai: The Japanese Secret to a Long and Happy Life',
+    rating: {
+      stars: 4.5,
+      count: 5200
+    },
+    priceCents: 3895
+  },
+  {
+    id: "garnier-charcoal-scrub-123456789",
+    image: "https://m.media-amazon.com/images/I/41hryFSg58L._SX300_SY300_QL70_FMwebp_.jpg",
+    name: 'Garnier SkinActive Charcoal Blackhead Eliminating Scrub',
+    rating: {
+      stars: 4,
+      count: 124
+    },
+    priceCents: 759
   }
+
 
 ];
 
